4373d2c3ed2ceac414b490269344bdeafa6119cf
Jexeones24 <jexeones@gmail.com>
2017-11-04 14:13:56 -0400

    fixed syntax errors in index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
-const bodyParser= require('body-parser')
-const path = require('path')
-const app = express();
-const data = require('./data')
+const bodyParser = require('body-parser')
+const app = express()
 const logic = require('./src/logic')
 
 const PORT = process.env.PORT || 3000
@@ -12,7 +10,6 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
-
 app.get('/', (req, res) => {
   res.render('index')
 })
@@ -21,17 +18,15 @@ app.get('/new', (req, res) => {
   res.render('new', { title: 'CREATE NEW WORKOUT' })
 })
 
-
 app.post('/new', function (req, res) {
-  let timeDomain =  Number(req.body.timeDomain);
+  let timeDomain = Number(req.body.timeDomain)
   res.render('show', {
     workoutObj: logic.makeWorkout(timeDomain)
   })
 })
 
-
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 })
 
-module.exports = app;
+module.exports = app
